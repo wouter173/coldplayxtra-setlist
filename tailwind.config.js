527876fff6c0971d4d5583ab366fcb0eba345384
wouter <wouter@debruijn.dev>
2022-02-05 15:53:56 +0100

    update typeface & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background-main': '#111111',
+        'accent-main': '#9d9d9d',
+      },
+      fontFamily: {
+        body: "'Nunito Sans', sans-serif",
+        head: "'Nunito Sans', sans-serif",
+      },
+    },
   },
   plugins: [],
 };
