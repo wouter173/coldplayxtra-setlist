cc9140f221404a60565e2b6526dae51dfb9743f4
wouter <wouter@debruijn.dev>
2022-02-06 18:15:38 +0100

    added button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import React, { PropsWithChildren } from 'react';
+
+type Props = {
+  outline?: boolean;
+  disabled?: boolean;
+};
+
+export default function Button(props: PropsWithChildren<Props>) {
+  let color = '';
+  if (props.outline) {
+    color += 'border border-2 ';
+    color += props.disabled ? 'border-accent-disabled' : 'border-accent-main active:border-accent-active';
+  } else {
+    color = props.disabled ? 'bg-accent-disabled' : 'bg-accent-main active:bg-accent-active';
+  }
+
+  return (
+    <button
+      disabled={props.disabled}
+      className={`${color} my-4 block w-full select-none rounded-xl p-3 font-body font-extrabold text-white transition-colors`}
+    >
+      {props.children}
+    </button>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
+import Button from '../components/button';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <p>wow</p>
-      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+    <div className="h-screen w-screen bg-background-main">
+      <div className="mx-auto w-10/12 py-12">
+        <Button>test button</Button>
+        <Button outline>test button</Button>
+        <Button disabled>test button</Button>
+        <Button outline disabled>
+          test button
+        </Button>
+      </div>
     </div>
   );
 };
