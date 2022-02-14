eeeb82d05910a2afe075d9abfdfbd1a08eab6d92
Sting Alleman <stingalleman@icloud.com>
2022-02-14 02:52:58 +0100

    add empty song

--- src/context/State.tsx
+++ src/context/State.tsx
@@ -47,6 +47,7 @@ const Provider = (props: PropsWithChildren<{}>) => {
         { id: GenerateID(), name: 'My Universe', customInfo: [] },
         { id: GenerateID(), name: 'A Sky Full of Stars', customInfo: [] },
         { id: GenerateID(), name: 'Coloratura', customInfo: [] },
+        { id: GenerateID(), name: '', customInfo: [] },
       ],
     },
   ]);
