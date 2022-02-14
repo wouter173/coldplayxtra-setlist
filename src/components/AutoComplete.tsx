728488d9cf28b56bdccf72f4751e4c2484255be0
Sting Alleman <stingalleman@icloud.com>
2022-02-14 01:38:05 +0100

    hide img if unreleased track

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -11,24 +11,31 @@ export default function AutoComplete() {
       {tracks
         .filter((track) => track.name.toUpperCase().includes(song.name.toUpperCase()))
         .slice(0, 10)
-        .map((track) => (
-          <li key={track.name} className="w-full">
-            <button
-              onMouseDown={() => {
-                setSong({ stage, setStages, stages, i, songName: track.name });
-              }}
-              className="w-full border-b border-gray-200 py-2 px-3 hover:bg-secondary-blue"
-            >
-              <div className="flex items-center">
-                <img className="h-14 w-14" alt={track.album + ' album cover'} src={track.artwork} />
-                <div className="px-3 py-2 text-left">
-                  <span className="text-base font-bold uppercase text-black">{track.name}</span>
-                  <span className="block text-sm uppercase text-gray-500">{track.album}</span>
+        .map((track) => {
+          let artwork;
+          if (track.artwork) {
+            artwork = <img className="h-14 w-14" alt={track.album + ' album cover'} src={track.artwork} />;
+          }
+
+          return (
+            <li key={track.name} className="w-full">
+              <button
+                onMouseDown={() => {
+                  setSong({ stage, setStages, stages, i, songName: track.name });
+                }}
+                className="w-full border-b border-gray-200 py-2 px-3 hover:bg-secondary-blue"
+              >
+                <div className="flex items-center">
+                  {artwork}
+                  <div className="px-3 py-2 text-left">
+                    <span className="text-base font-bold uppercase text-black">{track.name}</span>
+                    <span className="block text-sm uppercase text-gray-500">{track.album}</span>
+                  </div>
                 </div>
-              </div>
-            </button>
-          </li>
-        ))}
+              </button>
+            </li>
+          );
+        })}
     </ul>
   );
 }
