141342b4e80d0b3aa76a037d6ebf4b32fa3d1c4f
wouter <wouter@debruijn.dev>
2022-02-15 22:35:30 +0100

    added localstorage

--- src/context/State.tsx
+++ src/context/State.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, Dispatch, PropsWithChildren, SetStateAction, useState } from 'react';
+import React, { createContext, Dispatch, PropsWithChildren, SetStateAction, useEffect, useState } from 'react';
 import { template } from '../data/template';
 
 type contextType<T> = { [key: string]: [T, Dispatch<SetStateAction<T>>] };
@@ -12,9 +12,28 @@ export type stageType = { id: string; name: string; songs: songType[] };
 const context = createContext<contextType<any>>({});
 
 const Provider = (props: PropsWithChildren<{}>) => {
-  const name = useState('');
-  const stages = useState<stageType[]>(template());
-  return <context.Provider value={{ name, stages }}>{props.children}</context.Provider>;
+  const [name, setName] = useState('');
+  const [stages, setStages] = useState<stageType[]>(template());
+
+  useEffect(() => {
+    const stagesData = localStorage.getItem('stages');
+    const nameData = localStorage.getItem('name');
+
+    if (stagesData != null) setStages(JSON.parse(stagesData));
+    if (nameData != null) setName(JSON.parse(nameData));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('name', JSON.stringify(name));
+  }, [name]);
+
+  useEffect(() => {
+    localStorage.setItem('stages', JSON.stringify(stages));
+  }, [stages]);
+
+  return (
+    <context.Provider value={{ name: [name, setName], stages: [stages, setStages] }}>{props.children}</context.Provider>
+  );
 };
 
 export { Provider, context };
