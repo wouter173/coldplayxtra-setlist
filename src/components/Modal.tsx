7205ca1fb9a7d130ae03885add2437ba6ca1c44b
wouter <wouter@debruijn.dev>
2022-02-16 16:17:28 +0100

    eslint ignore fix

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -45,6 +45,7 @@ const Modal = (props: { title: string; onDismiss: () => void; component: JSX.Ele
     return () => {
       setActive(false);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <></>;
 };
