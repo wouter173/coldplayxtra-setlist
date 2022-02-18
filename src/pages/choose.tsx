bc8a6461395ebd5a995035fa3a4e0868aee6cf79
wouter <wouter@debruijn.dev>
2022-02-18 13:02:45 +0100

    new design for mobile

--- src/pages/choose.tsx
+++ src/pages/choose.tsx
@@ -1,25 +1,66 @@
+import { DocumentAddIcon, ViewListIcon } from '@heroicons/react/outline';
 import { useRouter } from 'next/router';
-import React, { useContext } from 'react';
-import Button from '../components/Button';
+import React, { ReactNode, useContext } from 'react';
 import Header from '../components/Header';
 import { context } from '../context/State';
+import { template } from '../data/template';
+
+const ChooseButton = (props: { onClick: () => void; img?: string; Icon?: ReactNode; text: string }) => {
+  const { onClick, img, Icon, text } = props;
+  return (
+    <button className="mb-6 w-full" onClick={() => onClick()}>
+      {img ? (
+        <div className="relative h-auto w-full" style={{ boxShadow: '0px 0px 20px rgba(255, 192, 203, 0.4)' }}>
+          <img
+            src={img}
+            alt="preview image"
+            className="w-full border-x border-t border-secondary-pink after:absolute after:block after:h-full after:w-full  after:bg-opacity-10"
+          />
+          <div className="absolute top-0 left-0 h-full w-full bg-[#FEC0FF] bg-opacity-20"></div>
+        </div>
+      ) : null}
+      <div className="flex w-full items-center justify-center border border-secondary-pink p-6 text-lg font-bold uppercase text-secondary-pink">
+        {Icon}
+        <span>{text}</span>
+      </div>
+    </button>
+  );
+};
 
 export default function Editor() {
   const router = useRouter();
   const states = useContext(context);
+  const [, setStages] = states.stages;
   const [name] = states.name;
 
   return (
-    <div className="bg-main h-screen min-h-fit w-screen">
+    <div className="bg-main h-fit min-h-screen w-screen">
       <div className="grid h-full w-full" style={{ gridTemplateRows: 'min-content max-content' }}>
         <Header />
-        <div className="row-span-4 mx-auto h-auto w-11/12 bg-secondary-purple p-8">
-          <div className="mx-auto text-center">
-            <h2 className="my-8 text-3xl font-bold">Hey, {name}!</h2>
-            <span className="mb-8 block">Step 2. How would you like to create your setlist?</span>
-            <Button>Use a template</Button>
-            <Button onClick={() => router.push('/editor')}>Start from scratch</Button>
-          </div>
+        <div className="mx-auto w-10/12 text-center text-white">
+          <h2 className="mt-4 text-3xl font-bold uppercase">Hey, {name} 👋</h2>
+          <span className="mb-8 block">how would you like to build your setlist?</span>
+        </div>
+        <div className="mx-auto w-11/12">
+          <ChooseButton
+            onClick={() => {
+              setStages(template());
+              router.push('/editor');
+            }}
+            img="/template_preview.png"
+            Icon={<ViewListIcon className="mr-1 h-6 w-6" />}
+            text="Use a template"
+          />
+
+          <ChooseButton
+            onClick={() => {
+              setStages([]);
+              router.push('/editor');
+            }}
+            img="/blank_preview.png"
+            Icon={<DocumentAddIcon className="mr-1 h-6 w-6" />}
+            text="Blank canvas"
+          />
         </div>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,93 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { useContext } from 'react';
+import { PropsWithChildren, useContext } from 'react';
 import Button from '../components/Button';
 import Header from '../components/Header';
 import Input from '../components/Input';
 import { context } from '../context/State';
 
+const HomeCard = ({ title, className, children }: PropsWithChildren<{ title?: string; className?: string }>) => (
+  <div className={`${className} mx-auto w-11/12 bg-white bg-opacity-5 p-6 text-white`}>
+    {title ? <h3 className="font-mono text-2xl font-bold uppercase">{title}</h3> : null}
+    <p className="font-mono">{children}</p>
+  </div>
+);
+
 const Home: NextPage = () => {
   const states = useContext(context);
   const router = useRouter();
+  const [stages] = states.stages;
 
   return (
-    <div className="bg-main h-screen min-h-fit w-screen">
-      <div className="grid h-full w-full" style={{ gridTemplateRows: 'min-content min-content max-content' }}>
+    <div className="bg-main min-h-fit w-screen">
+      <div className="min-h-full min-w-full pb-24">
         <Header />
-        <div className="mx-auto mt-20 w-10/12 pt-12 pb-24">
-          <h2 className="text-center text-sm font-bold text-white">Step 1. enter your name</h2>
+        <div className="mx-auto w-10/12 pt-12 pb-24">
+          <h2 className="my-10 text-center text-lg text-white">
+            Please enter your first name, or your @name, whichever you prefer!
+          </h2>
           <Input
-            className="mb-12 border-[#FFC0CB] text-3xl"
+            className="mb-12 border-b-4 border-[#FFC0CB] text-3xl placeholder:opacity-30"
             placeholder="Name..."
             centered
             type="text"
             state={states.name}
           />
           <Button
-            className="w-max border-[#FFC0CB] text-[#FFC0CB]"
+            className="mx-auto w-max border-[#FFC0CB] p-6 text-[#FFC0CB]"
             onClick={() => {
-              router.push('/choose');
+              if (stages.length > 0) router.push('/editor');
+              else router.push('/choose');
             }}
           >
             GET STARTED
           </Button>
         </div>
-        <div className="row-span-4 mx-auto h-auto w-10/12 bg-secondary-purple"></div>
+        <div>
+          <HomeCard title="Welcome 👋">
+            In anticipation of Coldplay’s upcomming Music of the Spheres World Tour, we thought it would be fun to think
+            about what the setlist might look like before big first show in Costa Rica on March 18
+          </HomeCard>
+          <img src="/coldplay_bg.jpg" alt="coldplay playing music" className="w-full" />
+          <HomeCard>
+            We had so much fun thinking about what the shows might look like, we wanted to make something that all fans
+            could enjoy. Presenting to you... the ColdplayXtra Setlist Maker!
+          </HomeCard>
+          <HomeCard title="About the tool ℹ️" className="mt-10">
+            The ColdplayXtra Setlist Maker makes it easy for you to make your dream Coldplay setlist
+            <br />
+            <br />
+            1. Get started by adding your name, or your @name, whatever you prefer! This will show up on your completed
+            setlist
+            <br />
+            <br />
+            2. Select either a template to work with, or a blank canvas to start from scratch
+          </HomeCard>
+          <img src="/preview_1.png" alt="preview of the tool" className="w-full" />
+          <HomeCard>
+            3. Select songs from the band’s discography and reorder them with ease. Customize songs with special
+            effects, such as pyro, lasers and of course, confetti.
+            <br />
+            <br />
+            4. You can add sections that note where the song will be played, such as the Main-stage, B-Stage and
+            C-Stages of the venue
+          </HomeCard>
+          <img src="/preview_2.png" alt="preview of the tool" className="w-full" />
+          <HomeCard className="mb-10">
+            5. When happy with the order and contents of your setlist, you can render a beautiful image that looks quite
+            similar to the setlists seen on-stage
+            <br />
+            <br />
+            6. Share your setlist with friends on Social Media
+          </HomeCard>
+          <img src="/preview_3.png" alt="preview of the tool" className="w-full" />
+          <HomeCard title="your setlist 📱️">
+            The images can then be shared on any social media wesbite or with friends to compare
+            <br />
+            <br />
+            We would like to thank Dutch duo @wouter and @sting for bringing the tool to life
+          </HomeCard>
+        </div>
       </div>
     </div>
   );
